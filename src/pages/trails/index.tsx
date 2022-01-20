e62b1741fb7b7d05b9a0905c98cd391945a791cb
Alexandre <alexandre.monteiro.bec@gmail.com>
2022-01-19 21:08:01 -0300

    chore: add Trails page

---
+++ src/pages/trails/index.tsx
@@ -0,0 +1,124 @@
+import ExpanseTrailIcon from '../../assets/icons/expanse-trail.svg';
+import ImpulseTrailIcon from '../../assets/icons/impulse-trail.svg';
+import OriginTrailIcon from '../../assets/icons/origin-trail.svg';
+import {
+  Container,
+  LogoIcon,
+  Main,
+  TrailsHeder,
+  TrailsHederHeadline,
+  Title,
+  TrailsBody,
+  TrailsBodyHeadline,
+  TrailsBodyParagraph,
+  TrailButton,
+  TrailCard,
+  TrailDescription,
+  TrailIconWrapper,
+  TrailLevel,
+  TrailsList,
+  TrailTitle,
+} from '../../styles/pages/trails';
+
+export default function Trails() {
+  return (
+    <Main>
+      <Container>
+        <TrailsHeder>
+          <TrailsHederHeadline>
+            <LogoIcon />
+
+            <Title>
+              Escolha a sua missão e <br /> conclua a sua inscrição
+            </Title>
+          </TrailsHederHeadline>
+        </TrailsHeder>
+      </Container>
+
+      <TrailsBody>
+        <Container>
+          <TrailsBodyHeadline>
+            <Title>
+              Trilhas <br /> disponíveis
+            </Title>
+
+            <TrailsBodyParagraph>
+              <strong>Tenha foco e não se preocupe:</strong> Você poderá trocar
+              de trilha em qualquer momento. Escolha apenas uma trilha para
+              absorver tudo a fundo. Falta de foco e conhecimento superficial é
+              o que vai te impedir de alcançar seus objetivos.
+            </TrailsBodyParagraph>
+          </TrailsBodyHeadline>
+
+          <TrailsList>
+            <TrailCard trail="origin">
+              <div>
+                <TrailIconWrapper>
+                  <OriginTrailIcon />
+                </TrailIconWrapper>
+
+                <TrailTitle trail="origin">
+                  Mission: <strong>Origin</strong>
+                </TrailTitle>
+
+                <TrailLevel trail="origin">Foundation</TrailLevel>
+
+                <TrailDescription>
+                  Se você está no início da jornada, com menos de um ano de
+                  estudo, e quer construir os alicerces necessários para decolar
+                  em programação, essa é a missão certa para você.
+                </TrailDescription>
+              </div>
+
+              <TrailButton trail="origin">confirmar embarque</TrailButton>
+            </TrailCard>
+
+            <TrailCard trail="impulse">
+              <div>
+                <TrailIconWrapper>
+                  <ImpulseTrailIcon />
+                </TrailIconWrapper>
+
+                <TrailTitle trail="impulse">
+                  Mission: <strong>Impulse</strong>
+                </TrailTitle>
+
+                <TrailLevel trail="impulse">Ignition</TrailLevel>
+
+                <TrailDescription>
+                  Se você já dominou os fundamentos da programação e quer levar
+                  suas habilidades para o próximo nível desenvolvendo uma
+                  aplicação completa, essa é a sua missão.
+                </TrailDescription>
+              </div>
+
+              <TrailButton trail="impulse">confirmar embarque</TrailButton>
+            </TrailCard>
+
+            <TrailCard trail="expanse">
+              <div>
+                <TrailIconWrapper>
+                  <ExpanseTrailIcon />
+                </TrailIconWrapper>
+
+                <TrailTitle trail="expanse">
+                  Mission: <strong>Expanse</strong>
+                </TrailTitle>
+
+                <TrailLevel trail="expanse">Advanced</TrailLevel>
+
+                <TrailDescription>
+                  Aulas avançadas e independentes para você descobrir como
+                  tecnologias modernas são aplicadas no dia-a-dia de empresas,
+                  para você expandir seu conhecimento.
+                </TrailDescription>
+              </div>
+
+              <TrailButton trail="expanse">confirmar embarque</TrailButton>
+            </TrailCard>
+          </TrailsList>
+        </Container>
+      </TrailsBody>
+    </Main>
+  );
+}
---
+++ src/styles/pages/trails.ts
@@ -0,0 +1,382 @@
+import styled from 'styled-components';
+
+import LogoSVG from '../../assets/icons/logo.svg';
+
+type TrailType = {
+  trail: 'origin' | 'impulse' | 'expanse';
+};
+
+const trails = {
+  origin: {
+    background: 'linear-gradient(90deg, #4763ff, rgba(45, 59, 138, 0.2))',
+    color: '#4863f7',
+  },
+  impulse: {
+    background: 'linear-gradient(90deg, #1b873f, rgba(27,135,63,.2))',
+    color: '#04d361',
+  },
+  expanse: {
+    background: 'linear-gradient(90deg, #d73628, rgba(215,54,40,.2))',
+    color: '#d73628',
+  },
+};
+
+export const Main = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  background-position: top;
+  background-repeat: no-repeat;
+  background-image: url('/images/trails/background-heat.png');
+
+  @media (min-width: 320px) {
+    background-size: cover;
+  }
+
+  @media (min-width: 768px) {
+    background-size: 100vw 360px;
+  }
+
+  @media (min-width: 992px) {
+    background-size: 100vw 336px;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+
+  @media (min-width: 320px) {
+    width: 100%;
+
+    margin: unset;
+  }
+
+  @media (min-width: 768px) {
+    margin: 0 24px;
+  }
+
+  @media (min-width: 1200px) {
+    width: 1120px;
+
+    margin: unset;
+  }
+`;
+
+export const TrailsHeder = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (min-width: 320px) {
+    align-items: unset;
+    flex-direction: column;
+
+    height: auto;
+
+    padding: 24px 24px 64px;
+  }
+
+  @media (min-width: 768px) {
+    height: 360px;
+  }
+
+  @media (min-width: 992px) {
+    align-items: center;
+    flex-direction: row;
+
+    height: 336px;
+
+    padding: 0 24px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: unset;
+  }
+`;
+
+export const TrailsHederHeadline = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LogoIcon = styled(LogoSVG)`
+  height: auto;
+
+  @media (min-width: 320px) {
+    width: 256px;
+
+    margin-bottom: 40px;
+  }
+
+  @media (min-width: 992px) {
+    width: 348px;
+  }
+
+  @media (min-width: 1200px) {
+    margin-bottom: 32px;
+  }
+`;
+
+export const Title = styled.h1`
+  font-weight: 700;
+
+  color: var(--gray);
+
+  @media (min-width: 320px) {
+    font-size: 30px;
+    line-height: 35px;
+
+    max-width: 270px;
+
+    margin-bottom: 32px;
+  }
+
+  @media (min-width: 425px) {
+    max-width: unset;
+  }
+
+  @media (min-width: 576px) {
+    max-width: 424px;
+  }
+
+  @media (min-width: 992px) {
+    font-size: 36px;
+    line-height: 46px;
+
+    margin-bottom: unset;
+  }
+`;
+
+export const TrailsBody = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+
+  padding-bottom: 160px;
+
+  background: linear-gradient(270deg, #09090a, #121214);
+`;
+
+export const TrailsBodyHeadline = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (min-width: 320px) {
+    align-items: unset;
+    flex-direction: column;
+
+    margin: 80px 24px 86px;
+  }
+
+  @media (min-width: 768px) {
+    margin: 80px 0 110px;
+  }
+
+  @media (min-width: 992px) {
+    flex-direction: row;
+  }
+`;
+
+export const TrailsBodyParagraph = styled.p`
+  @media (min-width: 320px) {
+    font-size: var(--font-size);
+    font-weight: 400;
+    line-height: 26px;
+
+    color: var(--gray);
+  }
+
+  @media (min-width: 992px) {
+    max-width: 456px;
+  }
+
+  @media (min-width: 1200px) {
+    line-height: 25px;
+
+    max-width: 576px;
+  }
+
+  strong {
+    font-weight: 700;
+
+    background-repeat: repeat;
+    background-size: 100%;
+    background-image: linear-gradient(
+      269.94deg,
+      #ff008e 71.42%,
+      #8257e5 99.94%
+    );
+
+    background-clip: text;
+
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+`;
+
+export const TrailsList = styled.ul`
+  display: grid;
+
+  @media (min-width: 320px) {
+    grid-gap: 70px;
+    gap: 70px;
+
+    grid-column: 1;
+    grid-template-columns: unset;
+  }
+
+  @media (min-width: 768px) {
+    grid-gap: 86px 32px;
+    gap: 86px 32px;
+
+    grid-column: unset;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
+
+export const TrailCard = styled.li<TrailType>`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+
+  position: relative;
+
+  padding: 74px 15px 32px 24px;
+
+  border: 1px solid var(--shape-hover);
+
+  background: linear-gradient(180deg, #202024, rgba(32, 32, 36, 0));
+
+  @media (min-width: 320px) {
+    border-radius: unset;
+
+    height: auto;
+  }
+
+  @media (min-width: 576px) {
+    border-radius: 5px;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 4px;
+
+    background: ${({ trail }) => trails[trail].background};
+
+    @media (min-width: 320px) {
+      border-radius: unset;
+    }
+
+    @media (min-width: 576px) {
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+    }
+  }
+`;
+
+export const TrailIconWrapper = styled.div`
+  svg {
+    position: absolute;
+    top: -46px;
+    left: 24px;
+  }
+`;
+
+export const TrailTitle = styled.h4<TrailType>`
+  font-weight: 400;
+
+  color: var(--gray);
+
+  @media (min-width: 320px) {
+    font-size: 30px;
+    line-height: 35px;
+
+    color: var(--gray);
+
+    margin-bottom: 4px;
+  }
+
+  @media (min-width: 576px) {
+    font-size: 32px;
+    line-height: 46px;
+
+    margin-bottom: unset;
+  }
+
+  strong {
+    color: ${({ trail }) => trails[trail].color};
+  }
+`;
+
+export const TrailLevel = styled.span<TrailType>`
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 23px;
+
+  color: ${({ trail }) => trails[trail].color};
+
+  margin-top: 8px;
+`;
+
+export const TrailDescription = styled.p`
+  @media (min-width: 320px) {
+    font-size: var(--font-size);
+    font-weight: 400;
+    line-height: 26px;
+
+    color: var(--gray);
+
+    margin-top: 24px;
+
+    max-width: 321px;
+  }
+
+  @media (min-width: 576px) {
+    max-width: unset;
+  }
+
+  @media (min-width: 1200px) {
+    line-height: 25px;
+
+    max-width: 304px;
+  }
+`;
+
+export const TrailButton = styled.button<TrailType>`
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 16px;
+
+  text-align: center;
+  text-transform: uppercase;
+
+  color: var(--white);
+
+  border: none;
+  border-radius: 5px;
+
+  width: 100%;
+  height: 56px;
+
+  background: ${({ trail }) => trails[trail].color};
+
+  @media (min-width: 320px) {
+    margin-top: 24px;
+  }
+`;
